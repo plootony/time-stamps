5f972e027e6d553318e7b180c038bef041af6b55
Tony <plootony.dev@gmail.com>
2024-05-08 15:49:05 +0300

    Добавил типизацию карточек на главной

--- src/components/ShowcaseCard.vue
+++ src/components/ShowcaseCard.vue
@@ -2,25 +2,29 @@
   <div class="card">
     <img
         class="card__image"
-        :src="`@/assets/images/${image}.jpg`"
+        :src="`src/assets/images/${card.image}.jpg`"
         width="200" height="170"
-        alt="card-image">
+        :alt="card.title">
 
     <div class="card__body">
-      <h2 class="card__title">{{ title }}</h2>
+      <h2 class="card__title">{{ card.title }}</h2>
 
-      <p class="card__subtitle">{{ desc }}</p>
+      <p class="card__subtitle">{{ card.desc }}</p>
 
       <div class="tags">
-        <div v-for="tag in tags" class="tag">
-          {{ tag }}
-        </div>
+        <span
+            v-for="tag in card.tags"
+            :key="tag"
+            class="tag"
+        >{{ tag }}
+        </span>
       </div>
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
 import type {ICard} from "@/interfaces/ICard";
 
-defineProps<ICard>()
+defineProps<{ card: ICard }>()
 </script>
--- src/interfaces/ICard.ts
+++ src/interfaces/ICard.ts
@@ -1,4 +1,5 @@
 export interface ICard {
+    id: number,
     image: string,
     title: string,
     desc: string,
