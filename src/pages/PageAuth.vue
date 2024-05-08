21f2698994c4d681e0da636bae71158ca72e1f94
Tony <plootony.dev@gmail.com>
2024-05-08 21:37:29 +0300

    Добавил динамическую форму авторизации/регистрации

--- src/pages/PageAuth.vue
+++ src/pages/PageAuth.vue
@@ -3,24 +3,37 @@
     <template #content>
       <div class="auth">
         <form class="auth-form">
-          <h2 class="auth-form__title">Авторизация</h2>
-          <p class="auth-form__subtitle">Введите логин и пароль для авторизации</p>
+          <h2 class="auth-form__title">{{ title }}</h2>
 
-          <div class="form-group">
+          <p class="auth-form__subtitle">{{ subtitle }}</p>
+
+          <div :class="['form-group', {'is-error' : isError}]">
             <label class="form-group__label">Введите логин</label>
+
             <input type="email" class="form-group__input" placeholder="name@mail.com">
-            <p v-if="false" class="form-group__error">Тут будет выводиться ошибка валидации, авторизации и тд</p>
+
+            <p v-if="isError" class="form-group__error">Тут будет выводиться ошибка валидации, авторизации и тд</p>
           </div>
 
-          <div class="form-group">
+          <div :class="['form-group', {'is-error' : isError}]">
             <label class="form-group__label">Введите пароль</label>
+
             <input type="password" class="form-group__input" placeholder="••••••••">
-            <p v-if="false" class="form-group__error">Тут будет выводиться ошибка валидации, авторизации и тд</p>
+
+            <p v-if="isError" class="form-group__error">Тут будет выводиться ошибка валидации, авторизации и тд</p>
           </div>
 
-          <button @click.prevent="console.log(1)" class="btn btn--primary" type="submit">Войти</button>
+          <div v-if="!isAuth" :class="['form-group', {'is-error' : isError}]">
+            <label class="form-group__label">Повторите пароль</label>
 
-          <p class="auth-form__register">Нет аккаунта? <span>Регистрация</span></p>
+            <input type="password" class="form-group__input" placeholder="••••••••">
+
+            <p v-if="isError" class="form-group__error">Тут будет выводиться ошибка валидации, авторизации и тд</p>
+          </div>
+
+          <button class="btn btn--primary" type="submit">{{ title }}</button>
+
+          <p @click="toggleAuth" class="auth-form__register">{{ link }}</p>
         </form>
       </div>
     </template>
@@ -29,4 +42,28 @@
 
 <script setup lang="ts">
 import GuestLayout from "@/layouts/GuestLayout.vue";
+import {computed, ref} from "vue";
+
+const isError = ref<boolean>(true)
+const isLoading = ref<boolean>(false)
+
+const isAuth = ref<boolean>(false)
+
+const toggleAuth = () => {
+  isAuth.value = !isAuth.value
+}
+
+const title = computed<string>(() => {
+  return isAuth.value ? 'Регистрация' : 'Авторизация'
+})
+
+const subtitle = computed<string>(() => {
+  return isAuth.value ? 'Введите данные для регистрации' : 'Введите данные для авторизации'
+})
+
+const link = computed<string>(() => {
+  return isAuth.value ? 'Уже есть аккаунт? Войти' : 'Нет аккаунта? Зарегистрироваться'
+})
+
 </script>
+e>
--- src/stores/user.ts
+++ src/stores/user.ts
@@ -2,7 +2,7 @@ import {ref} from 'vue'
 import {defineStore} from 'pinia'
 
 export const useUserStore = defineStore('user', () => {
-    const userId = ref<string>('')
+    const userId = ref<string>('1')
 
     return {userId}
 })
