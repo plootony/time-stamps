4518fd767b994eaaabf8bc19495de48af168cf40
Tony <plootony.dev@gmail.com>
2024-06-24 13:42:39 +0500

    Базовый компонент календаря

--- src/pages/PageCalendar.vue
+++ src/pages/PageCalendar.vue
@@ -2,20 +2,13 @@
   <main-layout>
     <template #content>
       <ScheduleXCalendar :calendar-app="calendarApp"/>
-
-      <form @submit.prevent="addEvent">
-        <input v-model="newEvent.title" placeholder="Название события"/>
-        <input v-model="newEvent.start" type="datetime-local" placeholder="Начало"/>
-        <input v-model="newEvent.end" type="datetime-local" placeholder="Конец"/>
-        <button type="submit">Добавить событие</button>
-      </form>
     </template>
 
   </main-layout>
 </template>
 
-<script setup lang="ts">
-import {reactive} from "vue"
+<script setup lang='ts'>
+import {reactive} from 'vue'
 import {ScheduleXCalendar} from '@schedule-x/vue'
 import {
   createCalendar,
@@ -25,7 +18,7 @@ import {
 } from '@schedule-x/calendar'
 import MainLayout from '@/layouts/MainLayout.vue'
 import {createEventsServicePlugin} from '@schedule-x/events-service'
-import { createEventModalPlugin } from '@schedule-x/event-modal'
+import {createEventModalPlugin} from '@schedule-x/event-modal'
 
 interface IEvent {
   id: number
@@ -33,6 +26,7 @@ interface IEvent {
   start: string
   end: string
 }
+
 const eventsServicePlugin = createEventsServicePlugin()
 const calendarApp = createCalendar({
   views: [viewDay, viewWeek, viewMonthGrid],
@@ -53,23 +47,13 @@ const calendarApp = createCalendar({
     },
   ] as IEvent[],
   plugins: [eventsServicePlugin, createEventModalPlugin()],
-})
 
+})
 const newEvent = reactive<IEvent>({
-  id: 0,
+  id: Date.now(),
   title: '',
   start: '',
   end: '',
 })
-
-
-// Тестовое добалвение
-const addEvent = () => {
-  newEvent.id = Date.now()
-  eventsServicePlugin.add({ ...newEvent })
-  newEvent.title = ''
-  newEvent.start = ''
-  newEvent.end = ''
-}
 </script>
 
