d96653f7b7df931650ec50677983eae7650bdcca
Tony <plootony.dev@gmail.com>
2024-07-05 16:25:08 +0500

    Добавил vuevalidate для страницы авторизации

--- src/pages/PageAuth.vue
+++ src/pages/PageAuth.vue
@@ -7,49 +7,75 @@
 
           <p class="auth-form__subtitle">{{ subtitle }}</p>
 
-          <div :class="['form-group', {'is-loading' : isLoading, 'is-error' : errors.email}]">
+          <div :class="['form-group', {'is-loading' : isLoading, 'is-error' : v.userEmail.$errors.length}]">
             <label class="form-group__label">Введите логин</label>
 
             <input
-                type="email"
-                class="form-group__input"
-                placeholder="name@mail.com"
-                v-model="email">
+              type="email"
+              class="form-group__input"
+              placeholder="name@mail.com"
+              v-model="v.userEmail.$model"
+            >
+            <div class="form-group__errors">
+                <span
+                  v-for="error in v.userEmail.$errors"
+                  :key="error.$uid"
+                  class="form-group__error"
+                > {{ error.$message }}</span>
+            </div>
           </div>
 
-          <div :class="['form-group', {'is-loading' : isLoading, 'is-error' : errors.password}]">
+          <div :class="['form-group', {'is-loading' : isLoading, 'is-error' : v.password.$errors.length}]">
             <label class="form-group__label">Введите пароль</label>
 
             <input
-                type="password"
-                class="form-group__input"
-                placeholder="••••••••"
-                v-model="password"
+              type="password"
+              class="form-group__input"
+              placeholder="••••••••"
+              v-model="v.password.$model"
             >
+
+            <div class="form-group__errors">
+                <span
+                  v-for="error in v.password.$errors"
+                  :key="error.$uid"
+                  class="form-group__error"
+                > {{ error.$message }}</span>
+            </div>
           </div>
 
-          <div v-if="!isAuth"
-               :class="['form-group', {'is-loading' : isLoading, 'is-error' : errors.password}]">
+          <div
+            v-if="!isAuth"
+            :class="['form-group', {'is-loading' : isLoading, 'is-error' : v.passwordConfirm.$errors.length}]"
+          >
             <label class="form-group__label">Повторите пароль</label>
 
             <input
-                type="password"
-                class="form-group__input"
-                placeholder="••••••••"
-                v-model="passwordConfirm"
+              type="password"
+              class="form-group__input"
+              placeholder="••••••••"
+              v-model="v.passwordConfirm.$model"
             >
+
+            <div class="form-group__errors">
+                <span
+                  v-for="error in v.passwordConfirm.$errors"
+                  :key="error.$uid"
+                  class="form-group__error"
+                > {{ error.$message }}</span>
+            </div>
           </div>
 
           <button
-              :class="['btn btn--primary', {'is-loading' : isLoading}]"
-              type="submit"
-              @click.prevent="submitForm"
+            :class="['btn btn--primary', {'is-loading' : isLoading}]"
+            type="submit"
+            @click.prevent="submitForm"
           >{{ title }}
           </button>
 
           <span
-              @click="toggleAuth"
-              class="auth-form__register"
+            @click="toggleAuth"
+            class="auth-form__register"
           >{{ link }}</span>
         </form>
       </div>
@@ -59,48 +85,62 @@
 
 <script setup lang='ts'>
 import {computed, ref} from 'vue'
-import GuestLayout from '@/layouts/GuestLayout.vue'
-import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth'
+import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 import {FirebaseError} from '@firebase/util'
 import {useRouter} from 'vue-router'
 import {toast} from 'vue3-toastify'
+import useVuelidate from '@vuelidate/core'
+import {helpers, minLength, required, maxLength, email, sameAs} from '@vuelidate/validators'
+import GuestLayout from '@/layouts/GuestLayout.vue'
 
 const router = useRouter()
 const isAuth = ref<boolean>(false)
 const isLoading = ref<boolean>(false)
 
-const email = ref<string>('')
+const userEmail = ref<string>('')
 const password = ref<string>('')
 const passwordConfirm = ref<string>('')
 
 interface IErrors {
-  email: boolean
+  userEmail: boolean
   password: boolean
 }
 
+const rules = computed(() => ({
+  userEmail: {
+    required: helpers.withMessage('Поле обязательно для заполнения', required),
+    email: helpers.withMessage('Вы ввели неверный email', email)
+  },
+  password: {
+    required: helpers.withMessage('Поле обязательно для заполнения', required),
+    minLength: helpers.withMessage(`Минимальная длина: 8 символа`, minLength(8)),
+    maxLength: helpers.withMessage(`Максимальная длина: 16 символа`, maxLength(16))
+  },
+  passwordConfirm: {
+    required: helpers.withMessage('Поле обязательно для заполнения', required),
+    sameAsPassword: helpers.withMessage(`Пароли не совпадают`, sameAs(password.value)),
+    minLength: helpers.withMessage(`Минимальная длина: 8 символа`, minLength(8)),
+    maxLength: helpers.withMessage(`Максимальная длина: 16 символа`, minLength(16))
+  }
+}))
+
+const v = useVuelidate(rules, {userEmail, password, passwordConfirm})
+
 const errors = ref<IErrors>({
-  email: false,
-  password: false,
+  userEmail: false,
+  password: false
 })
 
 const toggleAuth = (): void => {
   isAuth.value = !isAuth.value
   clearForm()
-  clearErrors()
 }
 
-//** Очищаем инптуы*/
+/** Очищаем инптуы*/
 const clearForm = (): void => {
-  email.value = ''
+  userEmail.value = ''
   password.value = ''
   passwordConfirm.value = ''
-
-}
-
-/** Очищаем ошибки */
-const clearErrors = (): void => {
-  errors.value.email = false
-  errors.value.password = false
 }
 
 /** Переключение форм */
@@ -113,31 +153,17 @@ const submitForm = (): void => {
   }
 }
 
-// TODO: Возможно стоит вывести ошибки непосредственно под инпутом
-
 /** Обработка ошибок */
 function authErrors(error: FirebaseError) {
   if (error) {
     switch (error.code) {
       case 'auth/email-already-in-use':
         toast.error('Такой email уже используется')
-        errors.value.email = true
+        errors.value.userEmail = true
         break
       case 'auth/invalid-credential':
         toast.error('Неверный логин или пароль')
-        errors.value.email = true
-        break
-      case 'auth/invalid-email':
-        toast.error('Недопустимый формат email')
-        errors.value.email = true
-        break
-      case 'auth/weak-password':
-        toast.error('Слабый пароль. Пароль должен быть не менее 6 символов')
-        errors.value.password = true
-        break
-      case 'auth/missing-password':
-        toast.error('Необходимо ввести пароль')
-        errors.value.password = true
+        errors.value.userEmail = true
         break
       default:
         toast.error('Произошла непредвиденная ошибка: ' + error.message)
@@ -150,7 +176,9 @@ function authErrors(error: FirebaseError) {
 
 /** Регистрация */
 const signUp = async (): Promise<void> => {
-  clearErrors()
+  v.value.$touch()
+
+  if (v.value.$error) return
 
   isLoading.value = true
 
@@ -161,7 +189,7 @@ const signUp = async (): Promise<void> => {
   }
 
   try {
-    await createUserWithEmailAndPassword(getAuth(), email.value, password.value)
+    await createUserWithEmailAndPassword(getAuth(), userEmail.value, password.value)
 
     router.push('/')
   } catch (error: unknown) {
@@ -177,11 +205,14 @@ const signUp = async (): Promise<void> => {
 
 /** Авторизация*/
 const signIn = async (): Promise<void> => {
-  clearErrors()
+  v.value.$touch()
+
+  if (v.value.$error) return
+
   isLoading.value = true
 
   try {
-    await signInWithEmailAndPassword(getAuth(), email.value, password.value)
+    await signInWithEmailAndPassword(getAuth(), userEmail.value, password.value)
 
     router.push('/')
   } catch (error: unknown) {
